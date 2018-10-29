359bc79056dc9d421c235744754b4a1e6d99ecef
jtfl2008 <jtfl2008@gmail.com>
2018-10-29 18:47:25 +0800

    冒泡排序

--- js/原生/算法类/bubbleSort.js
+++ js/原生/算法类/bubbleSort.js
@@ -1,9 +1,17 @@
 // 冒泡排序： 依次比较，交换相邻元素的大小
 var arr = [2, 9, 30, 0, 6]
 // 基础
+/**
+ * 冒泡排序
+ * @param
+ *  arr: [array]
+ * @returns
+ *  [array]
+ * by dongjianglong
+ */
 function bubbleSort(arr) {
   var len = arr.length
-  for (var i = 0; i < len; i++) {
+  for (var i = 0; i < len - 1; i++) {
     // 相邻元素两两比较
     for (var j = 0; j < len - 1 - i; j++) {
       // 元素交换
