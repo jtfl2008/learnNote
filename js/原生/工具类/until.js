773febb420ff5594fe0683b17b9878cff2bc898c
jtfl2008 <jtfl2008@gmail.com>
2018-10-30 12:46:43 +0800

    数组扁平化

--- js/原生/工具类/until.js
+++ js/原生/工具类/until.js
@@ -138,3 +138,17 @@ function arr2Obj(arr){
   });
   return obj
 }
+
+// flat
+
+function flat(arr){
+  var res = []
+  for(var i=0;i<arr.length;i++){
+    if(Array.isArray(arr[i])){
+      res = res.concat(flat(arr[i]))
+    }else{
+      res.push(arr[i])
+    }
+  }
+  return res
+}
