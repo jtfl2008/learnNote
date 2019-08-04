ff89b1e0e7590a10f05d271a20a4f51b0b202593
jtfl2008 <jtfl2008@gmail.com>
2019-08-04 16:10:40 +0800

    类数组转数组

---
+++ js/原生/工具类/函数参数转数组.js
@@ -0,0 +1,5 @@
+var args = Array.prototype.slice.call(arguments)
+var args = [].slice().call(arguments)
+var args = Array.apply(null, arguments)
+var args = Array.from(arguments)
+var args = [...arguments]
