3789a98498badd641ab913337ed0ae8a212b9cdb
dongjianglong <>
2022-09-29 17:55:01 +0800

    feat✨:  实现loader

---
+++ 工程化/loaders/babel-loader/index.js
@@ -0,0 +1,10 @@
+const schema = require('./schema.json');
+const babel = require('@babel/core');
+
+module.exports = function (content) {
+  const options = this.getOptions(schema);
+  const callback = this.async();
+  babel.transform(content, options, function (err, result) {
+    callback(err, result.code);
+  });
+};
